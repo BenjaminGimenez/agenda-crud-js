5d42b2438f04e307eb7dfbea6ac8db5b6387d4f7
Benjamin Gimenez <benjagimenez824@gmail.com>
2024-05-17 09:54:16 -0300

    inicio de proyecto, creando archivos y carpetas

---
+++ js/app.js
@@ -0,0 +1,18 @@
+//_________________________
+//SELECCIONAR LOS ELEMENTOS
+//_________________________
+
+const $form = document.getElementById("form-contacto");
+const $inputNombre = document.getElementById("inputNombre");
+const $inputNumero = document.getElementById("inputNumero");
+const $inputEmail = document.getElementById("inputEmail");
+const $inputImagen = document.getElementById("inputImagen");
+const $inputNotas = document.getElementById("inputNotas");
+
+//_________________________
+//EVENT LISTENER DEL SUBMIT
+//_________________________
+
+$form = document.addEventListener("submit", () => {});
+
+const hola = 0;
